11ec3d31b9a4bcb2c3f201d6d9c3f2b3bb7fae13
Trish Moreno <trishmoreno02@gmail.com>
2018-10-15 19:43:17 +0800

    week six: added sql file for week six

---
+++ SQL/week_six.sql
@@ -0,0 +1,301 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 15, 2018 at 01:41 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cybercoach`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `username`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'Admin1', 'admin1', '$2y$10$UsUQVpJQN3oVL55L0SxEUuA0fLRRqk/pTDR5l5EICJvhGYZI8gUfm', NULL, '2018-09-11 20:25:57', '2018-09-11 20:25:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_comments`
+--
+
+CREATE TABLE `forum_comments` (
+  `id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `thread_id` int(11) NOT NULL,
+  `user_type` varchar(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `forum_comments`
+--
+
+INSERT INTO `forum_comments` (`id`, `comment`, `user_id`, `thread_id`, `user_type`, `created_at`, `updated_at`) VALUES
+(1, 'topkek', 2, 1, 'teacher', '2018-10-15 02:52:51', '2018-10-15 02:52:51'),
+(2, 'topkek', 2, 1, 'student', '2018-10-15 02:55:45', '2018-10-15 02:55:45'),
+(3, 'topkek', 2, 1, 'student', '2018-10-15 02:57:08', '2018-10-15 02:57:08'),
+(4, 'topkek', 2, 1, 'student', '2018-10-15 02:57:51', '2018-10-15 02:57:51'),
+(5, 'topkek', 2, 1, 'student', '2018-10-15 02:58:14', '2018-10-15 02:58:14'),
+(6, 'kek', 2, 1, 'teacher', '2018-10-15 03:25:25', '2018-10-15 03:25:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_threads`
+--
+
+CREATE TABLE `forum_threads` (
+  `id` int(11) NOT NULL,
+  `thread_title` text NOT NULL,
+  `thread_body` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_id` int(11) NOT NULL,
+  `user_type` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `forum_threads`
+--
+
+INSERT INTO `forum_threads` (`id`, `thread_title`, `thread_body`, `created_at`, `updated_at`, `user_id`, `user_type`) VALUES
+(1, 'll', 'alskjflek', '2018-10-11 04:32:14', '2018-10-11 04:32:14', 2, 'student'),
+(2, 'yolo', 'yolo zzz', '2018-10-11 04:41:59', '2018-10-11 04:41:59', 2, 'student'),
+(3, 'discussion', 'lorem ipsum whatever lol aosilejfknor OIAELWJEWWL elkjfjdslfkjdknzxmn', '2018-10-15 03:39:22', '2018-10-15 03:39:22', 2, 'teacher');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lessons`
+--
+
+CREATE TABLE `lessons` (
+  `id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `lesson_path` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lessons`
+--
+
+INSERT INTO `lessons` (`id`, `teacher_id`, `lesson_path`, `created_at`, `updated_at`) VALUES
+(1, 2, 'lessons_2/blep.docx', '2018-09-24 07:39:29', '2018-09-24 07:39:29'),
+(2, 2, 'Moonlight Sonata p1.pdf', '2018-09-26 05:05:42', '2018-09-26 05:05:42'),
+(3, 2, 'CS Thesis Forms 2017.docx', '2018-10-04 06:36:54', '2018-10-04 06:36:54'),
+(4, 2, 'ACCOMPLISHMENT REPORT.docx', '2018-10-15 03:23:03', '2018-10-15 03:23:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `teacher` int(11) NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `username`, `password`, `teacher`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'Juan', 'Dela Cruz', 'jdelacruz', '$2y$10$P30HGYFZ62qU2bo6a5azmeZ5mggdMzGoeTgmb5ifkgeGng7HiOhNm', 2, '8DMCFRkpUOWFr1VMGAVG1DgmQ5BoY0CUwn5mrN9ybvJqBIXkA0mSxMPoIfZ8', '2018-09-11 18:16:46', '2018-09-11 18:16:46'),
+(3, 'John', 'Santos', 'jsantos', '$2y$10$pjEeXpjFKV9htXoIqcvtZufK86dZP2MW9QOjoErtkTeyU.S.bqRjm', 0, '6HxviWRv67z871LTzpOFUPx279T6p3RNNVrl5bQA3MBKUAnmCImeYwZwE25m', '2018-09-11 18:38:30', '2018-09-11 18:38:30'),
+(4, 'Trish', 'Moreno', 'tmoreno', '$2y$10$4KPh5EOpb2fiKGZhwsT/dOFcKYbwe8iS1oRDFi1G48p04spPeS7lG', 0, NULL, '2018-09-23 02:28:28', '2018-09-23 02:28:28'),
+(5, 'Test', 'ting', 'testing123', '$2y$10$qm8N3hBXpTkdZZylnwAXi.Nqd66TV5LHCBlIKUqZLO9F60oGgLHnW', 0, NULL, '2018-09-23 02:32:10', '2018-09-23 02:32:10'),
+(6, 'Test', 'Ting', 'testing1234', '$2y$10$lGjovsyU6a/f7khP2H/Zku5UX/.87xyQndmrvZMs7pQiuvkvgqzkm', 0, NULL, '2018-09-23 02:36:31', '2018-09-23 02:36:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`id`, `first_name`, `last_name`, `username`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'John', 'Santos', 'jsantos', '$2y$10$W/wRfxBFFka88EjGw6y9S.fgXajfXMO.gSdRKIrXLjZlyhtiutrzy', NULL, '2018-09-11 18:55:20', '2018-09-11 18:55:20'),
+(3, 'Teacher', 'Test', 'testteacher123', '$2y$10$FCJbYAOzcqSfhUic44n8gulDUoRuOvr7eM7rv5xVVxy2l47nOo/NO', NULL, '2018-09-23 02:37:07', '2018-09-23 02:37:07');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `admin_username_unique` (`username`);
+
+--
+-- Indexes for table `forum_comments`
+--
+ALTER TABLE `forum_comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `forum_threads`
+--
+ALTER TABLE `forum_threads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_username_unique` (`username`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `teacher_username_unique` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `forum_comments`
+--
+ALTER TABLE `forum_comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `forum_threads`
+--
+ALTER TABLE `forum_threads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
